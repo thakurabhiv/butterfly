a262938c2d6b0b5c59875a1400bacddc6abdcbfa
Abhishek Thakur <abhithakur.id@gmail.com>
2025-02-09 13:47:40 +0530

    Added toast rich color via app state, sql changes

--- src/lib/app/state.svelte.ts
+++ src/lib/app/state.svelte.ts
@@ -5,11 +5,13 @@ const LOGIN_STATE = $state({
 type AppStateType = {
     theme?: "light" | "dark" | "system",
     dateFormat?: string,
+    toastRichColors?: boolean,
     goServicePort?: number,
 };
 const APP_STATE: AppStateType = $state({
     theme: "dark",
     dateFormat: "DD-MM-YYYY hh:mm A",
+    toastRichColors: true,
     goServicePort: 8080,
 });
 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,11 @@
 	import '../app.css';
 	import { ModeWatcher } from "mode-watcher";
 	import { Toaster } from "$lib/components/ui/sonner/index";
+	import { APP_STATE } from "$lib/app/state.svelte";
 
 	let { children } = $props();
 </script>
 
-<Toaster richColors/>
+<Toaster richColors={APP_STATE.toastRichColors}/>
 <ModeWatcher/>
 {@render children()}
